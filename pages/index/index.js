fdef8496a99c4d100ca8231ed04fead92b453251
342556846@qq.com <342556846@qq.com>
2016-12-23 19:01:32 +0800

    rend by json

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,14 +1,115 @@
 //index.js
 //获取应用实例
 var app = getApp()
+
+var buttonList = [{
+  text: 'AC',
+  className: 'sub-op',
+  active: true
+}, {
+  text: '+/-',
+  className: 'sub-op',
+  active: false
+}, {
+  text: '%',
+  className: 'sub-op',
+  active: false
+}, {
+  text: '÷',
+  className: 'op',
+  active: false
+}, {
+  text: '7',
+  className: '',
+  active: false
+}, {
+  text: '8',
+  className: '',
+  active: false
+}, {
+  text: '9',
+  className: '',
+  active: false
+}, {
+  text: '×',
+  className: 'op',
+  active: true
+}, {
+  text: '4',
+  className: '',
+  active: false
+}, {
+  text: '5',
+  className: '',
+  active: false
+}, {
+  text: '6',
+  className: '',
+  active: false
+}, {
+  text: '-',
+  className: 'op',
+  active: false
+}, {
+  text: '1',
+  className: '',
+  active: false
+}, {
+  text: '2',
+  className: '',
+  active: false
+}, {
+  text: '3',
+  className: '',
+  active: false
+}, {
+  text: '+',
+  className: 'op',
+  active: false
+}, {
+  text: '0',
+  className: 'col-2',
+  active: false
+}, {
+  text: '.',
+  className: '',
+  active: false
+}, {
+  text: '=',
+  className: 'op',
+  active: false
+}]
+
 Page({
-  data: {},
-  //事件处理函数
-  click: function() {
-    
+  data: {
+    buttonList: buttonList
   },
-  onLoad: function () {
-    console.log('onLoad')
-    
+  onLoad: function() {
+    console.log('onLoad');
+    // update buttonList position
+  },
+  handleTouchStart: function(e) {
+    this.updateHover(true, e.touches[0]);
+  },
+  handleTouchMove: function(e) {
+    console.log(e.touches);
+    this.updateHover(true, e.touches[0]);
+  },
+  handleTouchEnd: function() {
+    this.updateHover(false);
+  },
+  handleTouchCancel: function() {
+    this.updateHover(false);
+  },
+  updateHover: function(turnOn, positon) {
+    var _this = this;
+    if (turnOn) {
+      console.log(positon);
+    } else {
+      this.data.buttonList.forEach(function(button) {
+        button.active = false;
+      });
+      this.setData(this.data);
+    }
   }
 })
